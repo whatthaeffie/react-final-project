afd9e2a77ad7353d2ef3443b703155e065edea98
whatthaeffie <aprilefff@gmail.com>
2020-12-24 12:46:24 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
     <h1>Weather App</h1>
     <footer>
       Open-sourced code created by Effie Fletcher{" "}
     <a href="https://github.com/whatthaeffie/react-final-project" target="_blank"> available on Github </a>
     </footer>
+    </div>
     </div>
   );
 }
