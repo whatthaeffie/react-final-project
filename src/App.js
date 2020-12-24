82d09935142cf0366743dcd3df5ee75e088cbd1e
whatthaeffie <aprilefff@gmail.com>
2020-12-24 10:54:19 -0500

    added code link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather App
+    <h1>Weather App</h1>
+    <footer>
+      Open-sourced code created by Effie Fletcher{" "}
+    <a href="https://github.com/whatthaeffie/react-final-project" target="_blank"> available on Github </a>
+    </footer>
     </div>
   );
 }
