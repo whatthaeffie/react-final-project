de339cbc65f36774634b20c48cb12e8a0c8b33ec
whatthaeffie <aprilefff@gmail.com>
2020-12-26 21:37:40 -0500

    fixed date

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function FormatDate(props) {
+export default function FormattedDate(props) {
   let days = [
     "Sunday",
     "Monday",
@@ -8,20 +8,21 @@ export default function FormatDate(props) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
+    "Saturday"
   ];
-  let day = days[props.data.getDay()];
+  let day = days[props.date.getDay()];
   let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
+  
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
-}
-  return (
-      <div> 
-          {day} {hours}:{minutes}
-      </div>
-  );
   }
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
+}
